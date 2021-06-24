f9fc146ce2cf348fe904873c256a54e543798be9
Mohammadmahdi Sefatzadeh <68275350+Adjective10111@users.noreply.github.com>
2021-06-24 23:18:41 +0430

    Update Federation_SETUP.sql
    Merged All

--- SQL/Federation_SETUP.sql
+++ SQL/Federation_SETUP.sql
@@ -5,17 +5,17 @@ CREATE TABLE PERSON (
     Birth_date date NOT NULL,
     Phone varchar(11),
     CONSTRAINT Person_PK PRIMARY KEY (National_code)
-)
+);
 
 CREATE TABLE REFEREE (
     National_code varchar(10) NOT NULL UNIQUE,
     FOREIGN KEY (National_code) REFERENCES PERSON(National_code)
-)
+);
 
 CREATE TABLE SUPERVISOR (
     National_code varchar(10) NOT NULL UNIQUE,
     FOREIGN KEY (National_code) REFERENCES PERSON(National_code)
-)
+);
 
 CREATE TABLE PLAYER (
     National_code varchar(10) NOT NULL UNIQUE,
@@ -24,9 +24,9 @@ CREATE TABLE PLAYER (
 
     CONSTRAINT PLAYER_PK
         PRIMARY KEY (National_code),
-    CONSTRAINT Person_FK
+    CONSTRAINT PlayerPerson_FK
         FOREIGN KEY (National_code) REFERENCES PERSON(National_code)
-)
+);
 
 CREATE TABLE TECHNICAL_STAFF (
     National_code varchar(10) NOT NULL UNIQUE,
@@ -34,9 +34,9 @@ CREATE TABLE TECHNICAL_STAFF (
 
     CONSTRAINT TECHNICAL_STAFF_PK
         PRIMARY KEY (National_code),
-    CONSTRAINT Person_FK
+    CONSTRAINT StaffPerson_FK
         FOREIGN KEY (National_code) REFERENCES PERSON(National_code)
-)
+);
 
 CREATE TABLE TEAM (
     Team_id varchar(10) NOT NULL UNIQUE,
@@ -46,7 +46,7 @@ CREATE TABLE TEAM (
 
     CONSTRAINT TEAM_PK
         PRIMARY KEY (Name)
-)
+);
 
 CREATE TABLE CONTRACT (
     Serial_no varchar(10) NOT NULL UNIQUE,
@@ -63,12 +63,8 @@ CREATE TABLE CONTRACT (
     CONSTRAINT Team_FK
         FOREIGN KEY (Team_id) REFERENCES TEAM(Team_id),
 
-    CONSTRAINT Valid_Staff CHECK (
-        Staff_NC in ((SELECT National_code FROM PLAYER) UNION
-                    (SELECT National_code FROM TECHNICAL_STAFF))
-        ),
     CONSTRAINT Valid_Dates CHECK (End_date > Start_date)
-)
+);
 
 CREATE TABLE STADIUM (
 	Stadium_id varchar(10) NOT NULL UNIQUE,
@@ -77,7 +73,7 @@ CREATE TABLE STADIUM (
 
 	CONSTRAINT Stadium_PK
 		PRIMARY KEY (Stadium_id)
-)
+);
 
 CREATE TABLE SEAT (
 	Stadium_id varchar(10) NOT NULL,
@@ -86,18 +82,18 @@ CREATE TABLE SEAT (
 
 	CONSTRAINT Seat_PK
 		PRIMARY KEY (Stadium_id,type),
-	CONSTRAINT Stadium_FK
+	CONSTRAINT SeatStadium_FK
 		FOREIGN KEY (Stadium_id) REFERENCES STADIUM(Stadium_id)
-)
+);
 
 CREATE TABLE STADIUM_LOCATION(
 	Stadium_id varchar(10) NOT NULL,
 	SLocation varchar(10) NOT NULL,
 	CONSTRAINT SLocation_PK
 		PRIMARY KEY(Stadium_id, SLocation),
-	CONSTRAINT Stadium_FK
+	CONSTRAINT locStadium_FK
 		FOREIGN KEY(Stadium_id) REFERENCES STADIUM(Stadium_id)
-)
+);
 
 CREATE TABLE LEAGUE(
     League_id varchar(10) NOT NULL UNIQUE,
@@ -108,7 +104,7 @@ CREATE TABLE LEAGUE(
         PRIMARY KEY (League_id),
     CONSTRAINT Champion_FK
         FOREIGN KEY (Champion) REFERENCES TEAM(Team_id)
-)
+);
 
 CREATE TABLE PARTICIPATE(
     Tsn varchar(10) NOT NULL UNIQUE,
@@ -118,9 +114,9 @@ CREATE TABLE PARTICIPATE(
         PRIMARY KEY (Tsn, Lsn),
     CONSTRAINT Participant
         FOREIGN KEY (Tsn) REFERENCES TEAM(Team_id),
-    CONSTRAINT League_FK
+    CONSTRAINT PartLeague_FK
         FOREIGN KEY (Lsn) REFERENCES LEAGUE(League_id)
-)
+);
 
 CREATE TABLE MATCH (
 	Match_id varchar(10) NOT NULL unique,
@@ -133,15 +129,15 @@ CREATE TABLE MATCH (
 
 	CONSTRAINT Match_PK
 		PRIMARY KEY (Match_id),
-	CONSTRAINT Stadium_FK
+	CONSTRAINT PlayedStadium_FK
 		FOREIGN KEY (Stadium_id) REFERENCES STADIUM(Stadium_id),
 	CONSTRAINT HomeTeam_FK
 		FOREIGN KEY (Home_team) REFERENCES TEAM(Team_id),
 	CONSTRAINT AwayTeam_FK
 		FOREIGN KEY (Away_team) REFERENCES TEAM(Team_id),
-	CONSTRAINT League_FK
+	CONSTRAINT UnderLeague_FK
 		FOREIGN KEY (League_id) REFERENCES LEAGUE(League_id)
-)
+);
 
 CREATE TABLE JUDGE (
     Referee_NC varchar(10) NOT NULL,
@@ -154,31 +150,31 @@ CREATE TABLE JUDGE (
         PRIMARY KEY (Referee_NC, Match_id),
     CONSTRAINT Ref_FK
         FOREIGN KEY (Referee_NC) REFERENCES REFEREE(National_code),
-    CONSTRAINT Match_FK
+    CONSTRAINT JudgeMatch_FK
         FOREIGN KEY (Match_id) REFERENCES MATCH(Match_id),
 
     CONSTRAINT Score_limit
-        CHECK (0 <= Score <= 10),
+        CHECK (0 <= Score AND Score <= 10),
     CONSTRAINT Ref_Positions
         CHECK (
 		Position in
                		('Referee', 'HeadLinesman', 'LineJudge', 'Umpire',
                 	'BackJudge', 'SideJudge', 'FieldJudge')
         )
-)
+);
 
 CREATE TABLE SUPERVISE (
     Match_id varchar(10) NOT NULL UNIQUE,
     SV_NC varchar(10) NOT NULL,
     Description varchar(MAX),
 
-    CONSTRAINT Judge_PK
+    CONSTRAINT SUPERVISE_PK
         PRIMARY KEY (Match_id),
     CONSTRAINT Supervisor_FK
         FOREIGN KEY (SV_NC) REFERENCES SUPERVISOR(National_code),
-    CONSTRAINT Match_FK
+    CONSTRAINT SVMatch_FK
         FOREIGN KEY (Match_id) REFERENCES MATCH(Match_id),
-)
+);
 
 CREATE TABLE EVENT (
     Player1_NC varchar(10) NOT NULL,
@@ -193,12 +189,12 @@ CREATE TABLE EVENT (
         FOREIGN KEY (Player1_NC) REFERENCES PLAYER(National_code),
     CONSTRAINT Player2_FK
         FOREIGN KEY (Player2_NC) REFERENCES PLAYER(National_code),
-    CONSTRAINT Match_FK
+    CONSTRAINT OccurredMatch_FK
         FOREIGN KEY (Match_id) REFERENCES MATCH(Match_id),
 
     CONSTRAINT Valid_Code
         CHECK (Code in ('GL', 'F', 'YC', 'RC'))
-)
+);
 
 CREATE TABLE PLAYED (
     Player_NC varchar(10) NOT NULL,
@@ -208,16 +204,16 @@ CREATE TABLE PLAYED (
 
     CONSTRAINT Played_PK
         PRIMARY KEY (Player_NC, Match_id),
-    CONSTRAINT Player_FK
+    CONSTRAINT PlayingPlayer_FK
         FOREIGN KEY (Player_NC) REFERENCES PLAYER(National_code),
-    CONSTRAINT Match_FK
+    CONSTRAINT InMatch_FK
         FOREIGN KEY (Match_id) REFERENCES MATCH(Match_id),
 
-    CONSTRAINT Score_limit
-        CHECK (0 <= Score <= 10),
+    CONSTRAINT PlayerScore_limit
+        CHECK (0 <= Score AND Score <= 10),
     CONSTRAINT Positions
         CHECK (
 		Position in ('GK', 'SW', 'CB', 'LB', 'LWB', 'RB','RWB',
                 		'DM', 'CM', 'AM', 'LW', 'RW', 'S', 'CF')
             )
-)
+);
